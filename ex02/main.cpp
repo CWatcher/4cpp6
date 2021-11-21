e3fa20cee56846de0f72a67e3d364121a6f25950
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-21 22:27:49 +0300

    [done] ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,5 +1,72 @@
 #include <iostream>
+#include <cstdlib>
+#include <typeinfo>
 
-int	main()
+class	Base {
+public:
+	virtual	~Base() {};
+};
+
+class	A: public Base {};
+class	B: public Base {};
+class	C: public Base {};
+
+Base*	generate( void )
 {
+	std::cout << "Generated: ";
+
+	switch ( rand() % 3 + 'A' )
+	{
+		case 'A':	std::cout << "A";
+					return new A();
+		case 'B':	std::cout << "B";
+					return new B();
+		case 'C':	std::cout << "C";
+					return new C();
+		default :	std::cout << "nothing";
+					return NULL;
+	}
+}
+void	identify( Base* p )
+{
+	if ( dynamic_cast< A* >( p ) )
+		std::cout << "A";
+	else if ( dynamic_cast< B* >( p ) )
+		std::cout << "B";
+	else if ( dynamic_cast< C* >( p ) )
+		std::cout << "C";
+}
+void	identify( Base& baseChild )
+{
+	try
+	{	( void ) dynamic_cast< A& >( baseChild );
+		std::cout << "A";
+		return;
+	}
+	catch( std::bad_cast & ) {}
+	try
+	{	( void ) dynamic_cast< B& >( baseChild );
+		std::cout << "B";
+		return;
+	}
+	catch( std::bad_cast & ) {}
+	try
+	{	( void ) dynamic_cast< C& >( baseChild );
+		std::cout << "C";
+		return;
+	}
+	catch( std::bad_cast & ) {}
+}
+
+int		main()
+{
+	srand( time( NULL ) );
+	for ( int i = 0; i < 9; i++ ) {
+		Base *p = generate();
+		std::cout << ". Identified from pointer: ";
+		identify( p );
+		std::cout << ". Identified from reference: ";
+		identify( *p );
+		std::cout << std::endl;
+	}
 }
